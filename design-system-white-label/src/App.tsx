215f7f41a224433a7c6bc7916f01200c478c31dc
Gizmo <bert@gizmostudios.net>
2023-03-02 16:38:37 +0100

    added design system

--- design-system-white-label/src/App.tsx
+++ design-system-white-label/src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import './App.scss';
 import Button from './components/Button/Button';
 import Paragraph from './components/Paragraph/Paragraph';
 import { Container } from './components/Container/Container';
 import { Header } from './components/Header/Header';
+import styles from './App.module.scss';
+import './theming/generator.scss';
 
 function App() {
   return (
-    <div className="root">
+    <div className={styles.App}>
       <Header>
         <Container>
             <h1>Design System White Label</h1>
         </Container>
       </Header>
 
-      <main data-mode="dark">
+      <main data-company="openweb" data-mode="dark">
         <Container>
           <article>
-            <h2>Checkout this stuff!</h2>
+            <h2>OpenWeb style</h2>
             <Paragraph>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Paragraph>
@@ -27,7 +28,29 @@ function App() {
         </Container>
       </main>
 
-      <section data-mode="light">
+      <section data-company="openweb" data-mode="light" style={{marginBottom: '3rem'}}>
+        <Container>
+          <article>
+            <Paragraph>And this is a bit with a light theme</Paragraph>
+            <Button>And a button on light mode</Button>
+          </article>
+        </Container>
+      </section>
+
+      <section data-company="pauwels" data-mode="dark">
+        <Container>
+          <article>
+            <h2>Pauwels section</h2>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Paragraph>
+          
+            <Button appearance="rounded">Dark mode</Button>
+          </article>
+        </Container>
+      </section>
+
+      <section data-company="pauwels" data-mode="light">
         <Container>
           <article>
             <Paragraph>And this is a bit with a light theme</Paragraph>
